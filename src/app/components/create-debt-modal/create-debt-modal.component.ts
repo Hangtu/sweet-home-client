614d3af0ed3c72ea7c8784a12ec2277fbd777efd
Hang Tu Wong Ley Franco <hangtuwlf@gmail.com>
2019-02-12 23:35:35 -0600

    modal

--- src/app/components/create-debt-modal/create-debt-modal.component.ts
+++ src/app/components/create-debt-modal/create-debt-modal.component.ts
@@ -22,8 +22,19 @@ export class CreateDebtModalComponent implements OnInit {
   }
 
   save(item) {
+    const nombre = this.nombre.nativeElement.checkValidity();
+    const deuda = this.deuda.nativeElement.checkValidity();
+    const pago_min = this.pago_min.nativeElement.checkValidity();
     const limit_day = this.limit_day.nativeElement.checkValidity();
-    this.voted.emit(item);
+    if (nombre && deuda && pago_min && limit_day) {
+      this.voted.emit(item);
+    }
+  }
+
+  checkEmpty(event) {
+    const limit_day = this.limit_day.nativeElement.checkValidity();
+    console.log(limit_day);
+    console.log(this.limit_day.nativeElement.validity);
   }
 
 }
